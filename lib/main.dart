a18272e7baacdc98616f2b3ea24f26c05b642524
Rohit Chandra <chandrarohit91@gmail.com>
2021-06-14 21:38:23 +0530

    Adding,Images,GoogleFonts, ElevatedButton,Login Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:shopping_app/pages/home_page.dart';
 import 'package:shopping_app/pages/login_page.dart';
 
@@ -13,7 +14,10 @@ class ShoppingApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       themeMode: ThemeMode.light,
-      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        fontFamily: GoogleFonts.lato().fontFamily,
+      ),
       darkTheme: ThemeData(brightness: Brightness.dark),
       initialRoute: "/login",
       routes: {
