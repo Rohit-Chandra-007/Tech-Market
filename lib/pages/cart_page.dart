52a512dc0687af1f46d9defda7fdef1b8b0db939
Rohit Chandra <chandrarohit91@gmail.com>
2021-06-22 13:17:22 +0530

    Day 22 - Placeholder | Cart Page

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -12,6 +12,75 @@ class CartPage extends StatelessWidget {
         backgroundColor: Colors.transparent,
         title: "Cart".text.make(),
       ),
+      body: Column(
+        children: [
+          CartList().p32().expand(),
+          Divider(),
+          CartTotal(),
+        ],
+      ),
     );
+  }
+}
+
+class CartTotal extends StatelessWidget {
+  const CartTotal({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 200,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Text(
+            "\$999",
+            style: TextStyle(
+              fontSize: 32,
+              color: context.accentColor,
+            ),
+          ),
+          SizedBox(
+            width: 30,
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all(
+                context.theme.buttonColor,
+              ),
+            ),
+            child: Text(
+              "Buy",
+            ),
+          ).w32(context),
+        ],
+      ),
+    );
+  }
+}
+
+class CartList extends StatefulWidget {
+  const CartList({Key? key}) : super(key: key);
+
+  @override
+  _CartListState createState() => _CartListState();
+}
+
+class _CartListState extends State<CartList> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        itemCount: 5,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text('Item ${index + 1}'),
+            leading: Icon(Icons.done),
+            trailing: IconButton(
+              icon: Icon(Icons.remove_circle_outline),
+              onPressed: () {},
+            ),
+          );
+        });
   }
 }
